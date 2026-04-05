d206cbc848cf05c74ad488a7d19aea4022ec2de5
DinhMaiMTD <142642094+DinhMaiMTD@users.noreply.github.com>
2026-04-06 00:01:19 +0700

    fix(ckb): repeat 2 times transaction when send to myself - make protobuf

--- rust/trezor-client/src/protos/generated/messages.rs
+++ rust/trezor-client/src/protos/generated/messages.rs
@@ -581,8 +581,6 @@ pub enum MessageType {
     MessageType_CKBTxAckOutput = 5505,
     // @@protoc_insertion_point(enum_value:hw.trezor.messages.MessageType.MessageType_CKBTxAckCellDep)
     MessageType_CKBTxAckCellDep = 5506,
-    // @@protoc_insertion_point(enum_value:hw.trezor.messages.MessageType.MessageType_CKBSignedTx)
-    MessageType_CKBSignedTx = 5507,
     // @@protoc_insertion_point(enum_value:hw.trezor.messages.MessageType.MessageType_BenchmarkListNames)
     MessageType_BenchmarkListNames = 9100,
     // @@protoc_insertion_point(enum_value:hw.trezor.messages.MessageType.MessageType_BenchmarkNames)
@@ -883,7 +881,6 @@ impl ::protobuf::Enum for MessageType {
             5504 => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckInput),
             5505 => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckOutput),
             5506 => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckCellDep),
-            5507 => ::std::option::Option::Some(MessageType::MessageType_CKBSignedTx),
             9100 => ::std::option::Option::Some(MessageType::MessageType_BenchmarkListNames),
             9101 => ::std::option::Option::Some(MessageType::MessageType_BenchmarkNames),
             9102 => ::std::option::Option::Some(MessageType::MessageType_BenchmarkRun),
@@ -1173,7 +1170,6 @@ impl ::protobuf::Enum for MessageType {
             "MessageType_CKBTxAckInput" => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckInput),
             "MessageType_CKBTxAckOutput" => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckOutput),
             "MessageType_CKBTxAckCellDep" => ::std::option::Option::Some(MessageType::MessageType_CKBTxAckCellDep),
-            "MessageType_CKBSignedTx" => ::std::option::Option::Some(MessageType::MessageType_CKBSignedTx),
             "MessageType_BenchmarkListNames" => ::std::option::Option::Some(MessageType::MessageType_BenchmarkListNames),
             "MessageType_BenchmarkNames" => ::std::option::Option::Some(MessageType::MessageType_BenchmarkNames),
             "MessageType_BenchmarkRun" => ::std::option::Option::Some(MessageType::MessageType_BenchmarkRun),
@@ -1462,7 +1458,6 @@ impl ::protobuf::Enum for MessageType {
         MessageType::MessageType_CKBTxAckInput,
         MessageType::MessageType_CKBTxAckOutput,
         MessageType::MessageType_CKBTxAckCellDep,
-        MessageType::MessageType_CKBSignedTx,
         MessageType::MessageType_BenchmarkListNames,
         MessageType::MessageType_BenchmarkNames,
         MessageType::MessageType_BenchmarkRun,
@@ -1757,13 +1752,12 @@ impl ::protobuf::EnumFull for MessageType {
             MessageType::MessageType_CKBTxAckInput => 274,
             MessageType::MessageType_CKBTxAckOutput => 275,
             MessageType::MessageType_CKBTxAckCellDep => 276,
-            MessageType::MessageType_CKBSignedTx => 277,
-            MessageType::MessageType_BenchmarkListNames => 278,
-            MessageType::MessageType_BenchmarkNames => 279,
-            MessageType::MessageType_BenchmarkRun => 280,
-            MessageType::MessageType_BenchmarkResult => 281,
-            MessageType::MessageType_TelemetryGet => 282,
-            MessageType::MessageType_Telemetry => 283,
+            MessageType::MessageType_BenchmarkListNames => 277,
+            MessageType::MessageType_BenchmarkNames => 278,
+            MessageType::MessageType_BenchmarkRun => 279,
+            MessageType::MessageType_BenchmarkResult => 280,
+            MessageType::MessageType_TelemetryGet => 281,
+            MessageType::MessageType_Telemetry => 282,
         };
         Self::enum_descriptor().value_by_index(index)
     }
@@ -1782,7 +1776,7 @@ impl MessageType {
 }
 
 static file_descriptor_proto_data: &'static [u8] = b"\
-    \n\x0emessages.proto\x12\x12hw.trezor.messages\x1a\roptions.proto*\xebb\
+    \n\x0emessages.proto\x12\x12hw.trezor.messages\x1a\roptions.proto*\xc7b\
     \n\x0bMessageType\x12(\n\x16MessageType_Initialize\x10\0\x1a\x0c\x80\xa6\
     \x1d\x01\xb0\xb5\x18\x01\x90\xb5\x18\x01\x12\x1e\n\x10MessageType_Ping\
     \x10\x01\x1a\x08\x80\xa6\x1d\x01\x90\xb5\x18\x01\x12%\n\x13MessageType_S\
@@ -2098,16 +2092,15 @@ static file_descriptor_proto_data: &'static [u8] = b"\
     \xff*\x1a\x04\x98\xb5\x18\x01\x12$\n\x19MessageType_CKBTxAckInput\x10\
     \x80+\x1a\x04\x90\xb5\x18\x01\x12%\n\x1aMessageType_CKBTxAckOutput\x10\
     \x81+\x1a\x04\x90\xb5\x18\x01\x12&\n\x1bMessageType_CKBTxAckCellDep\x10\
-    \x82+\x1a\x04\x90\xb5\x18\x01\x12\"\n\x17MessageType_CKBSignedTx\x10\x83\
-    +\x1a\x04\x98\xb5\x18\x01\x12)\n\x1eMessageType_BenchmarkListNames\x10\
-    \x8cG\x1a\x04\x80\xa6\x1d\x01\x12%\n\x1aMessageType_BenchmarkNames\x10\
-    \x8dG\x1a\x04\x80\xa6\x1d\x01\x12#\n\x18MessageType_BenchmarkRun\x10\x8e\
-    G\x1a\x04\x80\xa6\x1d\x01\x12&\n\x1bMessageType_BenchmarkResult\x10\x8fG\
-    \x1a\x04\x80\xa6\x1d\x01\x12'\n\x18MessageType_TelemetryGet\x10\xcc\x08\
-    \x1a\x08\x80\xa6\x1d\x01\x90\xb5\x18\x01\x12$\n\x15MessageType_Telemetry\
-    \x10\xcd\x08\x1a\x08\x80\xa6\x1d\x01\x98\xb5\x18\x01\x1a\x08\xc8\xf3\x18\
-    \x01\xd0\xf3\x18\x01\"\x04\x08Z\x10\\\"\x04\x08G\x10J\"\x04\x08r\x10z\"\
-    \x05\x08{\x10\x95\x01\"\x06\x08\xdb\x01\x10\xdb\x01\"\x06\x08\xe0\x01\
+    \x82+\x1a\x04\x90\xb5\x18\x01\x12)\n\x1eMessageType_BenchmarkListNames\
+    \x10\x8cG\x1a\x04\x80\xa6\x1d\x01\x12%\n\x1aMessageType_BenchmarkNames\
+    \x10\x8dG\x1a\x04\x80\xa6\x1d\x01\x12#\n\x18MessageType_BenchmarkRun\x10\
+    \x8eG\x1a\x04\x80\xa6\x1d\x01\x12&\n\x1bMessageType_BenchmarkResult\x10\
+    \x8fG\x1a\x04\x80\xa6\x1d\x01\x12'\n\x18MessageType_TelemetryGet\x10\xcc\
+    \x08\x1a\x08\x80\xa6\x1d\x01\x90\xb5\x18\x01\x12$\n\x15MessageType_Telem\
+    etry\x10\xcd\x08\x1a\x08\x80\xa6\x1d\x01\x98\xb5\x18\x01\x1a\x08\xc8\xf3\
+    \x18\x01\xd0\xf3\x18\x01\"\x04\x08Z\x10\\\"\x04\x08G\x10J\"\x04\x08r\x10\
+    z\"\x05\x08{\x10\x95\x01\"\x06\x08\xdb\x01\x10\xdb\x01\"\x06\x08\xe0\x01\
     \x10\xe0\x01\"\x06\x08\xac\x02\x10\xb0\x02\"\x06\x08\xb5\x02\x10\xb8\x02\
     \"\x06\x08\xbc\x05\x10\xc5\x05\"\x06\x08\xe9\x07\x10\xf7\x07\"\x06\x08\
     \xfa\x07\x10\xcb\x08B8\n#com.satoshilabs.trezor.lib.protobufB\rTrezorMes\
